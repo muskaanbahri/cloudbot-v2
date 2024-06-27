d9341039cab214ff5a5969986a35ce1e73c30f90
muskaan-bit34 <muskaanbahri21@gmail.com>
2024-06-27 10:09:45 +0530

    commit2

--- src/pages/RealEstate.jsx
+++ src/pages/RealEstate.jsx
@@ -1,17 +1,21 @@
-import { Box, Button, Container, Stack, Typography } from "@mui/material";
+import { Box, Button, Container, Stack, Typography, Card, CardContent } from "@mui/material";
 import sampleImage from '../assets/sampleimg.jpg';
 import LibraryAddCheckIcon from '@mui/icons-material/LibraryAddCheck';
 import CircleIcon from '@mui/icons-material/Circle';
-
+import MouseIcon from '@mui/icons-material/Mouse';
+import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
+import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
+import NotificationsIcon from '@mui/icons-material/Notifications';
 
 
 
 function RealEstate() {
     return (
         <>
-        <Box sx={{ width: 1}}>
-            <Stack disableGutters style={{ width: '100%'}}>
-                <Stack  alignItems={"flex-start"}  flexDirection={"row"} paddingTop={10} style={{ width: '100%'}}>
+        <Stack  display="flex"  sx={{width: "100%"}} margin={0}>
+        
+            <Stack >
+                <Stack  alignItems={"flex-start"}  flexDirection={"row"} paddingTop={10} paddingBottom={5} style={{ width: '100%'}}>
                     <Stack spacing={2}  >
                         <Typography variant="h3">Scale your Real Estate Business with WhatsApp Automations</Typography>
                         <Stack  flexDirection={"row"}>
@@ -36,16 +40,16 @@ function RealEstate() {
                         </Stack>
                     </Stack>
                     
-                    <Stack spacing={4}>
+                    <Stack spacing={2}>
                         <img src={sampleImage} alt="Sample Image" style={{ width: '600px', height: '350px' }} />
                     </Stack>
                     
                 </Stack>
             </Stack>
-        </Box>
         
         
-        <Box sx={{ width: 1, background: "#E9FCD4", display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100px' }}>
+        
+        <Box sx={{  background: "#e9fae3", display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100px', borderRadius: "50px"}}>
                 <Typography variant="h6" component="span">
                     Leading real estate businesses 
                 </Typography>
@@ -57,7 +61,7 @@ function RealEstate() {
                 </Typography>
         </Box>
         //one more section
-        <Box paddingTop={10} paddingBottom={10} sx={{ width: 1, background: '#AAF27F', display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100px' }}>
+        <Box paddingTop={10} paddingBottom={10} paddingRight={5} paddingLeft={5} sx={{ width: 1, background: '#AAF27F', display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100px', borderRadius: "50px"         }}>
             <Typography variant="h3" ><center>Leverage the WhatsApp Business Platform with Interakt to acquire, engage & convert prospect buyers and retain customers. </center> </Typography>   
         </Box>
         
@@ -65,7 +69,7 @@ function RealEstate() {
 
         <Box sx={{ width: 1 }}>
             <Stack disableGutters style={{ width: '100%'}}>
-                <Stack  alignItems={"flex-start"}  flexDirection={"row"} paddingTop={5} style={{ width: '100%'}}>
+                <Stack  alignItems={"flex-start"}  flexDirection={"row"} paddingTop={10} style={{ width: '100%'}}>
                     <Stack spacing={2}  >
                         <Typography variant="h3">Generate more leads for your real estate business with WhatsApp</Typography>
                         <Stack  flexDirection={"row"}>
@@ -157,9 +161,177 @@ Nurture long-distance leads with virtual property tours on WhatsApp​</Typograp
             </Stack>
             
         </Box>
+        <Box sx={{ width: 1 }}>
+            <Stack disableGutters style={{ width: '100%'}} paddingTop={10} >
+                <Stack  alignItems={"flex-start"}  flexDirection={"row"} paddingTop={5} style={{ width: '100%'}}>
+                    <Stack spacing={2}  >
+                        <Typography variant="h3">Schedule and confirm site visits on WhatsApp
+                        </Typography>
+                        <Stack  flexDirection={"row"}>
+                            < CircleIcon fontSize="small"  color="success"/>
+                            <Typography paddingLeft={2} >
+                            Follow-up with prospect buyers to schedule site visits</Typography>
+                        </Stack>
+                        <Stack  flexDirection={"row"}>
+                            < CircleIcon fontSize="small"  color="success"/>
+                            <Typography paddingLeft={2} >Set up WhatsApp chatbots to request preferred day and time with prospects</Typography>
+                        </Stack>
+                        <Stack  flexDirection={"row"}>
+                            < CircleIcon fontSize="small"  color="success"/>
+                            <Typography paddingLeft={2} >Schedule the site visit and assign real estate agents for walkthroughs
+                            </Typography>
+                        </Stack>
+                        <Stack  flexDirection={"row"}>
+                            < CircleIcon  fontSize="small"  color="success"/>
+                            <Typography paddingLeft={2} >
+Set up site visit reminders and easy rescheduling to reduce last-minute drop-offs
+</Typography>
+                        </Stack>
+                        
+                    </Stack>
+                    
+                    <Stack spacing={4} paddingLeft={3}>
+                        <img src={sampleImage} alt="Sample Image" style={{ width: '600px', height: '350px' }} />
+                    </Stack>
+                </Stack>
+            </Stack>
+            
+        </Box>
+        <Box sx={{ width: 1 }}>
+            <Stack disableGutters style={{ width: '100%', background:"#e9fae3"}} marginTop={5} borderRadius={"50px"} padding={5} >
+                <Stack  alignItems={"flex-start"}  flexDirection={"row"} paddingTop={5} style={{ width: '100%'}}>
+                    <Stack spacing={2}  >
+                        <Typography variant="h3">
+                        Simplify document submission for prospect buyers
+                        </Typography>
+                        <Stack  flexDirection={"row"}>
+                            < CircleIcon fontSize="small"  color="success"/>
+                            <Typography paddingLeft={2} >
+                            Request documents for buyer verification using WhatsApp chatbots</Typography>
+                        </Stack>
+                        <Stack  flexDirection={"row"}>
+                            < CircleIcon fontSize="small"  color="success"/>
+                            <Typography paddingLeft={2} >Collect documents on WhatsApp over an encrypted channel
+                            </Typography>
+                        </Stack>
+                        <Stack  flexDirection={"row"}>
+                            < CircleIcon fontSize="small"  color="success"/>
+                            <Typography paddingLeft={2} >Categorize and save documents in your CRM
+                            
+                            </Typography>
+                        </Stack>
+                        <Stack paddingTop={0.5}>
+                            <Stack flexDirection={"row"} marginTop={3} >
+                                <Button variant="contained" color="success" style={{ marginRight: '15px' }} >Signup for free trial</Button>
+                                
+                            </Stack>
+                        </Stack>
+                        
+                        
+                    </Stack>
+                    
+                    <Stack spacing={4} paddingLeft={3}>
+                        <img src={sampleImage} alt="Sample Image" style={{ width: '600px', height: '350px' }} />
+                    </Stack>
+                </Stack>
+            </Stack>
+            
+        </Box>
 
+        <Box sx={{ width: '100%', padding: 4}}>
+            <Typography variant="h3" align="center" gutterBottom paddingTop={3} paddingBottom={5}>
+                Create delightful experiences on WhatsApp
+            </Typography>
+            <Stack direction="row" spacing={4} justifyContent="center" alignItems="flex-start">
+                <Card 
+                    sx={{ 
+                        backgroundColor: '#e9fae3', 
+                        padding: 2,
+                        '&:hover': {
+                            transform: 'scale(1.05)',
+                            transition: 'transform 0.3s ease-in-out',
+                            backgroundColor: "#D0F2FF"
+                        }
+                    }}
+                >
+                    <CardContent>
+                        <Stack spacing={1} alignItems="center">
+                            <MouseIcon fontSize="large" color="primary" />
+                            <Typography variant="h6">Acquire</Typography>
+                            <Typography align="center">
+                                Integrate Google & FB Leads form to obtain leads. Answer FAQs quickly via custom auto-replies and help clients in choosing the right home & book site visits.
+                            </Typography>
+                        </Stack>
+                    </CardContent>
+                </Card>
+                <Card 
+                    sx={{ 
+                        backgroundColor: '#e9fae3', 
+                        padding: 2,
+                        '&:hover': {
+                            transform: 'scale(1.05)',
+                            transition: 'transform 0.3s ease-in-out',
+                            backgroundColor: "#D0F2FF"
+                        }
+                    }}
+                >
+                    <CardContent>
+                        <Stack spacing={1} alignItems="center">
+                            <AccountBalanceWalletIcon fontSize="large" color="secondary" />
+                            <Typography variant="h6">Convert</Typography>
+                            <Typography align="center">
+                                Send reminders for site visits. Upsell & Cross-sell with personalized plumbing, furniture, etc recommendations on WhatsApp.
+                            </Typography>
+                        </Stack>
+                    </CardContent>
+                </Card>
+                <Card 
+                    sx={{ 
+                        backgroundColor: '#e9fae3', 
+                        padding: 2,
+                        '&:hover': {
+                            transform: 'scale(1.05)',
+                            transition: 'transform 0.3s ease-in-out',
+                            backgroundColor: "#D0F2FF"
+                        }
+                    }}
+                >
+                    <CardContent>
+                        <Stack spacing={1} alignItems="center">
+                            <EmojiEmotionsIcon fontSize="large" color="success" />
+                            <Typography variant="h6">Delight</Typography>
+                            <Typography align="center">
+                                Solve client queries quickly and at scale on WhatsApp. Gather ratings/feedback from clients using WhatsApp automated flows.
+                            </Typography>
+                        </Stack>
+                    </CardContent>
+                </Card>
+                <Card 
+                    sx={{ 
+                        backgroundColor: '#e9fae3', 
+                        padding: 2,
+                        '&:hover': {
+                            transform: 'scale(1.05)',
+                            transition: 'transform 0.3s ease-in-out',
+                            backgroundColor: "#D0F2FF"
+                        }
+                    }}
+                >
+                    <CardContent>
+                        <Stack spacing={1} alignItems="center">
+                            <NotificationsIcon fontSize="large" color="info" />
+                            <Typography variant="h6">Engage</Typography>
+                            <Typography align="center">
+                                Send festive and other discount offers to drive purchase interest. Send personalized property recommendations in their preferred location.
+                            </Typography>
+                        </Stack>
+                    </CardContent>
+                </Card>
+            </Stack>
+        </Box>
+        
 
-
+        </Stack>
         </>
         
         
